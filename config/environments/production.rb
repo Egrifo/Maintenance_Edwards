021aaf619063bd252737b3efda08d8ef323883f5
Egrifo <efisio.gigliotti@gmail.com>
2020-07-14 05:48:33 +0100

    changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
     config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'scialati.herokuapp.com/', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'egigliotti.herokuapp.com/', :protocol => 'https' }
 end
