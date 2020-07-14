9dbf5fb8152565b0b1ccd97c17a1265b35cb9068
Egrifo <efisio.gigliotti@gmail.com>
2020-07-14 05:26:42 +0100

    removed concerns

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 #validates_presence_of :name
  #def first_name
  # 	self.name.split.first
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
- Rails.application.config.assets.precompile += %w(jquery.js application.scss)
+ Rails.application.config.assets.precompile += %w(jquery.js jquery.min.js bootstrap.bundle.min.js collapsible.js service.scss service.js cocoon.js application.scss)
